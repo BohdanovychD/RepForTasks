5c2c5f8933362a4a1a528ce9c2df6f9d3ca4d282
BohdanovychD <dima.bogdanovych2001@gmail.com>
2021-06-27 12:54:33 +0300

    Homework Java game (lesson 2)

--- src/main/java/homework/lesson_2/Main.java
+++ src/main/java/homework/lesson_2/Main.java
@@ -1,4 +1,11 @@
 package homework.lesson_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+
+        controller.gameWithRandomNumber();
+    }
 }
---
+++ src/test/java/homework/lesson_2/ControllerTest.java
@@ -0,0 +1,12 @@
+package homework.lesson_2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void gameWithRandomNumber() {
+    }
+}
